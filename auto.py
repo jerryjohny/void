d40d8e6ce0d53ffa7482ca01fc41044256b87429
Germano <johnyjerry53@gmail.com>
2024-04-12 00:01:49 +0200

    tentativa de implementação de um teste de responsividade (não completo)

--- auto.py
+++ auto.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.chrome.options import Options
 
 
 driver = webdriver.Chrome()
-
-# Para abrir a página
 driver.get("https://void.co.mz/")
 
 
+#teste de formulário
 try:
     field_nome = driver.find_element(By.ID, 'form-field-name')
     field_nome.send_keys("Germano")
@@ -28,7 +28,35 @@ except Exception as e:
     print("Erro ao extrair:", e)
 
 finally:
-    # Fecha o navegador
-    driver.quit()
+    pass
 
-# nome = field_nome.text.strip()
+
+#Teste de responsividade
+driver.set_window_size(height=930,width=200)
+
+#clicka no menu blog
+blog = driver.find_element(By.XPATH,'//*[@id="menu-item-55"]/a/span[1]/span')
+blog.click()
+time.sleep(1)
+
+#clicka no menu jobar
+jobar = driver.find_element(By.XPATH,'//*[@id="menu-item-56"]/a/span[1]/span')
+jobar.click()
+time.sleep(1)
+
+#clicka no menu oi
+oi = driver.find_element(By.XPATH,'//*[@id="menu-item-57"]/a/span[1]/span')
+oi.click()
+time.sleep(1)
+
+#clicka no menu home
+home = driver.find_element(By.XPATH,'//*[@id="menu-item-58"]/a/span[1]/span')
+home.click()
+
+time.sleep(10)
+
+driver.set_window_size(height=930,width=700)
+
+driver.quit()
+
+
